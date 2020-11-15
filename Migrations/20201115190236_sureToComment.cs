e2f03541cd097c23733e9df8320a2b0a75243396
Arsène Lapostolet <arsene@lapostolet.fr>
2020-11-15 20:03:08 +0100

    atelier -> comment

---
+++ Migrations/20201115190236_sureToComment.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RencontreContemporainesAPI.Migrations
+{
+    public partial class sureToComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sure",
+                table: "Ateliers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Ateliers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Ateliers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Sure",
+                table: "Ateliers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
