69d50323e5dbc61f60b5d10495deb8f7771ae713
Ombrelin <arsene@lapostolet.fr>
2021-04-10 11:26:50 +0200

    add migration

---
+++ Migrations/20210410092534_hours.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RencontreContemporainesAPI.Migrations
+{
+    public partial class hours : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<TimeSpan>>(
+                name: "Hours",
+                table: "Concerts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hours",
+                table: "Concerts");
+        }
+    }
+}
